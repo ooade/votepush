797003a765382adcd1621e55a5bc2a70a3e52907
Adegbuyi Ademola Oluwamayowa <marhyorh@gmail.com>
2015-10-12 14:48:44 +0100

    Polls added to DB

--- client/app/polls/polls.controller.js
+++ client/app/polls/polls.controller.js
@@ -1,21 +1,70 @@
 'use strict';
 
 angular.module('votingappApp')
-  .controller('PollsCtrl', function ($scope, $http, Auth) {
+  .controller('PollsCtrl', function ($scope, $routeParams, $http, Auth, $window) {
 	$scope.isLoggedIn = Auth.isLoggedIn;
 	$scope.placeholders = ["Bacon","Tuna"];
 	$scope.options = [];
+	$scope.dbdata = [];
+	$scope.myPolls = [];
 	
-	$scope.moreOptions = function(){
-	  this.placeholders.push("New option");
+	var wallOwner = $routeParams.fullName;
+	var fullName = Auth.getCurrentUser().name;
+	
+	$scope.deletePoll = function(id){
+		$http.delete('/api/polls/' + id, function(){
+	    $window.location.href = '/polls/';
+		});
 	};
 	
-    $http.get('/api/polls').success(function(poll) {
-      $scope.poll = poll;
-    });
+	$scope.moreOptions = function(){
+  		this.placeholders.push("New option");
+	};	
+	
+	$http.get('/api/polls/' + fullName).success(function(polls){
+  		$scope.myPolls = polls;	
+		if(polls.length < 1){
+			$scope.isPoll = false;
+		}
+		else{
+			$scope.isPoll = true;
+		}
+	});
 	
 	$scope.addNew = function(){
 		var pollName = $scope.pollName;
 		var options = $scope.options;
+		$http.post('/api/polls', {name:pollName, fullName:fullName, options:options}).success(function(poll) {
+	 	$window.location.href = '/polls/'+fullName+'/'+pollName;
+		});
 	}
-  });
+
+//	$http.get('/api/polls').success(function(polls) {
+//      $scope.dbdata = polls;
+//    });
+	
+	/** Below is strictly for the charts **/
+	
+	var data = [
+    {
+        value: 2,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 3,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 5,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+];
+//var ctx = document.getElementById("myChart").getContext("2d");
+//var myDoughnutChart = new Chart(ctx).Doughnut(data);
+});
--- client/app/polls/polls.js
+++ client/app/polls/polls.js
@@ -3,12 +3,16 @@
 angular.module('votingappApp')
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/polls', {
-        templateUrl: 'app/polls/polls.html',
-        controller: 'PollsCtrl'
-      })
 	  .when('/polls/new', {
         templateUrl: 'app/polls/new.html',
         controller: 'PollsCtrl'
-      });
+      })
+      .when('/polls/:fullName/:title', {
+         templateUrl: 'app/polls/polls.html',
+        controller: 'PollsCtrl'       
+      })
+	  .otherwise({
+		templateUrl: 'app/polls/polls.html',
+        controller: 'PollsCtrl'
+	 });
   });
